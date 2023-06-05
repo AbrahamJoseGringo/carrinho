85d56e70174e7a294f42cd20c8258b3fc6c23117
viniciuseduuu <viniciuseduardohilario@gmail.com>
2023-06-05 11:46:01 -0300

    h

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <script setup>
-import { livros } from '@/_data/livros.js';
-import { 
-  carrinho,
-  removerItemCarrinho,
-  atualizaQuantidadeItem
- } from '@/_data/carrinho.js';
- 
- import CardLivro from '@/components/CardLivro.vue';
- import MeuCarrinho from '@/components/MeuCarrinho.vue';
-
-function formatarPreco(preco) {
-    return 'R$ ' + preco.toFixed(2).replace('.', ',')
-  }
+ import ListaLivro from '@/components/ListaLivro.vue'
+ import MeuCarrinho from '@/components/MeuCarrinho.vue'
 </script>
 
 <template>
   <h1>Minha livraria</h1>
   <div class="container-geral">
-    <div class="listagem-livros">
-     <card-livro v-for="livro in livros" :key="livro.id" :livro="livro"/>
-    </div>
+    <lista-livro />
     <meu-carrinho />
   </div>
 </template>
@@ -32,11 +19,4 @@ function formatarPreco(preco) {
   display: grid;
   grid-template-columns: 3fr 1fr;
 }
-
-
-.listagem-livros {
-  display: flex;
-  flex-wrap: wrap;
-}
-
 </style>
