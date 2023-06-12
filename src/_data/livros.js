da2f2e72bfde3b2c4a8bd061fa54b4f70de97735
viniciuseduuu <viniciuseduardohilario@gmail.com>
2023-06-12 11:48:40 -0300

    holi

--- src/_data/livros.js
+++ src/_data/livros.js
@@ -22,17 +22,17 @@ export const livros = [
   },
   {
     id: 4,
-    title: 'Dom Quixote',
-    author: 'Miguel de Cervantes',
-    price: 39.9,
-    img: 'https://m.media-amazon.com/images/I/513uHO8IDlL._SX359_BO1,204,203,200_.jpg'
+    title: 'Em águas profundas',
+    author: 'F.T. Lukens',
+    price: 22.99,
+    img: 'https://m.media-amazon.com/images/I/51RdjiQfkVL._SX328_BO1,204,203,200_.jpg'
   },
   {
     id: 5,
-    title: 'O Conde de Monte Cristo',
-    author: 'Alexandre Dumas',
-    price: 69.9,
-    img: 'https://m.media-amazon.com/images/I/81ZswN9PVPL._AC_UL320_.jpg'
+    title: 'De sangue e cinzas (Vol. 1)',
+    author: 'Jennifer L. Armentrout',
+    price: 38.99,
+    img: 'https://m.media-amazon.com/images/I/5150My6ZzUL._SX342_BO1,204,203,200_.jpg'
   },
   {
     id: 6,
@@ -42,42 +42,42 @@ export const livros = [
     img: 'https://m.media-amazon.com/images/I/91M9xPIf10L._AC_UL320_.jpg'
   },
   {
-    id: 1,
+    id: 7,
     title: 'O Poderoso Chefão',
     author: 'Mario Puzo',
     price: 49.9,
     img: 'https://m.media-amazon.com/images/I/31jGFI5wX2L._SX331_BO1,204,203,200_.jpg'
   },
   {
-    id: 2,
+    id: 8,
     title: 'O Senhor dos Anéis - Box completo',
     author: 'J. R. R. Tolkien',
     price: 129.9,
     img: 'https://m.media-amazon.com/images/I/715afDdgKfL.jpg'
   },
   {
-    id: 3,
+    id: 9,
     title: 'O Pequeno Príncipe',
     author: 'Antoine de Saint-Exupéry',
     price: 19.9,
     img: 'https://m.media-amazon.com/images/I/41afCn3PQUL._SY344_BO1,204,203,200_QL70_ML2_.jpg'
   },
   {
-    id: 4,
+    id: 10,
     title: 'Dom Quixote',
     author: 'Miguel de Cervantes',
     price: 39.9,
     img: 'https://m.media-amazon.com/images/I/513uHO8IDlL._SX359_BO1,204,203,200_.jpg'
   },
   {
-    id: 5,
+    id: 11,
     title: 'O Conde de Monte Cristo',
     author: 'Alexandre Dumas',
     price: 69.9,
     img: 'https://m.media-amazon.com/images/I/81ZswN9PVPL._AC_UL320_.jpg'
   },
   {
-    id: 6,
+    id: 12,
     title: 'O Hobbit',
     author: 'J. R. R. Tolkien',
     price: 39.9,
---
+++ src/components/MButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    texto: String
+})
+</script>
+
+<template>
+    <button>{{ props.texto }}</button>
+</template>
+
+<style>
+buttom {
+    background-color: blue;
+    color: white;
+    font-size: 1.1rem;
+    border: 1px solid #ccc;
+    border-radius:  5px;
+    padding: 5px 10px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: rgb(255, 255, 255);
+}
+</style>
